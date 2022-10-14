29430a8c0811ea90418b19705f1aca91eebdccb4
abhishekgupta3247 <93422053+abhishekgupta3247@users.noreply.github.com>
2022-10-14 09:58:31 +0530

    stack using array code

---
+++ 24_stack_using_array.c
@@ -0,0 +1,49 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+struct stack{
+    int size;
+    int top;
+    int *arr; // Integer Pointer
+};
+
+int isEmpty(struct stack *ptr){
+    if(ptr->top == -1){
+        return 1;
+    }
+    else{
+        return 0;
+    }
+}
+
+int isFull(struct stack *ptr){
+    if(ptr->top == ptr->size-1){
+        return 1;
+    }
+    else{
+        return 0;
+    }
+}
+
+int main(){
+    // struct stack s;
+    // s.size = 80;
+    // s.top = -1;
+    // s.arr = (int *)malloc(s.size * sizeof(int));
+
+    // struct stack *s;
+    struct stack *s = (struct stack *)malloc(sizeof(struct stack));
+    // s = (struct stack *)malloc(sizeof(struct stack));
+    s->size = 80;
+    s->top = -1;
+    s->arr = (int *)malloc(s->size * sizeof(int));
+
+    // Check if stack is empty
+    if(isEmpty(s)){
+        printf("The stack is empty");
+    }
+    else{
+        printf("The stack is not empty");
+    }
+    return 0;
+}
